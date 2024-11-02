40b654358b49fc11afa9648634bca50630892ebc
DavidG9999 <bulbasik01@gmail.com>
2024-11-03 01:55:53 +0300

    Add grpc-server implementation

---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,109 @@
+package auth
+
+import (
+	"context"
+	"errors"
+
+	ssov1 "github.com/DavidG9999/api/gen/go/sso"
+	"github.com/DavidG9999/my_grpc_app/internal/services/auth"
+	"github.com/DavidG9999/my_grpc_app/internal/storage"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	emptyValue = 0
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth auth.Auth
+}
+
+func Register(gPRC *grpc.Server, auth auth.Auth) {
+	ssov1.RegisterAuthServer(gPRC, &serverAPI{auth: auth})
+}
+
+func (s *serverAPI) SignUp(ctx context.Context, req *ssov1.SignUpRequest) (*ssov1.SignUpResponse, error) {
+	if err := validateSighUp(req); err != nil {
+		return nil, err
+	}
+
+	userId, err := s.auth.SighUp(ctx, req.GetName(), req.GetEmail(), req.GetPassword(), req.GetIsAdmin())
+	if err != nil {
+		if errors.Is(err, auth.ErrUserExist) {
+			return nil, status.Error(codes.AlreadyExists, "user already exists")
+		}
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+	return &ssov1.SignUpResponse{
+		UserId: userId,
+	}, nil
+}
+
+func (s *serverAPI) SignIn(ctx context.Context, req *ssov1.SignInRequest) (*ssov1.SignInResponse, error) {
+	if err := validateSighIn(req); err != nil {
+		return nil, err
+	}
+	token, err := s.auth.SignIn(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		if errors.Is(err, auth.ErrInvalidCredentials) {
+			return nil, status.Error(codes.InvalidArgument, "user not found")
+		}
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+	return &ssov1.SignInResponse{
+		Token: token,
+	}, nil
+}
+
+func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			return nil, status.Error(codes.NotFound, "user not found")
+		}
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+	return &ssov1.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
+}
+
+func validateSighIn(req *ssov1.SignInRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password  is required")
+	}
+	if req.GetAppId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+	return nil
+}
+
+func validateSighUp(req *ssov1.SignUpRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password  is required")
+	}
+	if req.GetName() == "" {
+		return status.Error(codes.InvalidArgument, "name is required")
+	}
+	return nil
+}
+
+func validateIsAdmin(req *ssov1.IsAdminRequest) error {
+	if req.GetUserId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "user id is required")
+	}
+
+	return nil
+}
